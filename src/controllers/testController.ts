1d17264e08ca2c40ff0eaa88876616affc3476c3
lucasbrandao <brandslucas@gmail.com>
2022-09-17 18:06:36 -0300

    get tests grouping by teachers

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -14,5 +14,6 @@ export async function getTestsByDiscipline(req:Request, res: Response) {
 }
 
 export async function getTestsByTeacher(req:Request, res: Response) {
-    
+    const result = await testServices.getAllTestsByTeachers()
+    res.status(200).send(result)
 }
